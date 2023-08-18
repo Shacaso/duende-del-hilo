2d8b17f62687dd4bfb3136db50b2b4f39468f5c5
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-08-18 18:43:51 -0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className='m-10 btn btn-primary btn-wide'>Hi!</button>
+    </>
+  )
+}
+
+export default App
