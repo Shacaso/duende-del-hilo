29c73b35f06a3ebfbb8bdafdea3b9e2527fac666
Rodrigo Xavier Vargas <rxvargas.92@gmail.com>
2023-12-29 12:53:09 -0300

    agrego tipado

--- client-next/src/app/api/costumes/route.ts
+++ client-next/src/app/api/costumes/route.ts
@@ -2,9 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import { costumesPath } from "@/app/lib/data/paths";
 import { allEntities } from "@/app/lib/data/GetAndSaveJson";
+import { Costume } from "@/app/lib/definitions";
 
 export async function GET(req: NextApiRequest) {
-  const costumes = await allEntities(costumesPath)
+  const costumes: Costume[] = await allEntities(costumesPath)
   return NextResponse.json(costumes)
 }
 
