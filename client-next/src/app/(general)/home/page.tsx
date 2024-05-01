a4e40014126019b679505898c72175ee328f556b
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-05-01 11:21:49 -0300

    commit: fix bug

--- client-next/src/app/(general)/home/page.tsx
+++ client-next/src/app/(general)/home/page.tsx
@@ -1,21 +1,13 @@
-// import { Container } from "@/components";
 import { Welcome, Stat, DashboardPanel } from "./components";
 import {
-  // ProductIcon,
-  ProviderIcon,
+  CategoryIcon,
   BellSVG,
   IconEntrada,
   UserIcon,
   BillsIcon,
-  HistoricalIcon,
-  // IconSalida,
 } from "@/assets/svg";
 import ProductsDashboard from "./components/ProductsDashboard";
 import Link from "next/link";
-import NotificationDash from "./components/NotificationDash";
-// import Link from "next/link";
-// import NotificationDash from "@/components/notificacion-dash/NotificationDash";
-// import { ProductsDashboard } from "./components/ProductsDashboard";
 
 export default function Dashboard() {
   return (
@@ -28,20 +20,8 @@ export default function Dashboard() {
           Icon={IconEntrada}
           url={"/product"}
         />
-        {/* <Stat
-          title='Clientes'
-          // stat={providers.length}
-          Icon={ProviderIcon}
-          url={"/provider"}
-          // loading={loadingProviders}
-        /> */}
-        <Stat
-          title='Pendinetes de devolucion'
-          // stat={movements.filter(m => m.tipo === 'SALIDA').length}
-          Icon={HistoricalIcon}
-          url={"/history"}
-        />
-        <Stat title='Facturas' Icon={BillsIcon} url={"/history"} />
+        <Stat title='Categorias' Icon={CategoryIcon} url={"/categories"} />
+        <Stat title='Facturas' Icon={BillsIcon} url={"/bill"} />
       </div>
 
       <div className=' flex gap-5'>
@@ -65,13 +45,16 @@ export default function Dashboard() {
           Icon={BellSVG}
           isProduct={false}
         >
-          <DashboardPanel.Content>
+          {/* <DashboardPanel.Content>
             <NotificationDash />
-          </DashboardPanel.Content>
+          </DashboardPanel.Content> */}
 
           <DashboardPanel.Footer>
             <div className='flex flex-col gap-3 mt-5'>
-              <Link href={"/notification"} className='w-full btn btn-primary'>
+              <Link
+                href={"/notification"}
+                className='btn-disabled w-full btn btn-primary'
+              >
                 Ver más notificaciones
               </Link>
             </div>
@@ -79,66 +62,5 @@ export default function Dashboard() {
         </DashboardPanel>
       </div>
     </div>
-    // <div className='dashboard-page'>
-    //   {/* <Container> */}
-    //     <Welcome />
-    //     <div className='box-border flex flex-col justify-center w-full gap-5 mb-5 starts-group'>
-    //       <Stat
-    //         title='Nueva factura'
-    //         // stat={products.length}
-    //         Icon={IconEntrada}
-    //         url={"/product"}
-    //       />
-    //       {/* <Stat
-    //         title='Clientes'
-    //         stat={providers.length}
-    //         Icon={ProviderIcon}
-    //         url={'/provider'}
-    //         loading={loadingProviders}
-    //       /> */}
-    //       <Stat
-    //         title='Pendinetes de devolucion'
-    //         // stat={movements.filter(m => m.tipo === 'SALIDA').length}
-    //         Icon={HistoricalIcon}
-    //         url={"/history"}
-    //       />
-    //       <Stat title='Facturas' Icon={BillsIcon} url={"/history"} />
-    //     </div>
-    //     <div className='flex flex-col gap-5 mb-5 dashboard-panels md:flex-row '>
-    //       {/* <DashboardPanel
-    //         title={"Últimas facturas registradas"}
-    //         Icon={UserIcon}
-    //         isProduct={true}
-    //       >
-    //         <DashboardPanel.Content>
-    //           <ProductsDashboard />
-    //         </DashboardPanel.Content>
-    //         <DashboardPanel.Footer>
-    //           <Link href={"/product"} className='w-full mt-5 btn btn-primary'>
-    //             Ver más facturas
-    //           </Link>
-    //         </DashboardPanel.Footer>
-    //       </DashboardPanel>
-
-    //       <DashboardPanel
-    //         title={"Notificaciones"}
-    //         Icon={BellSVG}
-    //         isProduct={false}
-    //       >
-    //         <DashboardPanel.Content>
-    //           <NotificationDash />
-    //         </DashboardPanel.Content>
-
-    //         <DashboardPanel.Footer>
-    //           <div className='flex flex-col gap-3 mt-5'>
-    //             <Link href={"/notification"} className='w-full btn btn-primary'>
-    //               Ver más notificaciones
-    //             </Link>
-    //           </div>
-    //         </DashboardPanel.Footer>
-    //       </DashboardPanel> */}
-    //     </div>
-    //   {/* </Container> */}
-    // </div>
   );
 }
